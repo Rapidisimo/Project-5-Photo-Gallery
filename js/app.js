675a40ac16703ea13e82ac249ea1ee6302bd30de
Josue R <13837690+Rapidisimo@users.noreply.github.com>
2022-01-13 23:00:03 -0500

    CSS Tweaks and added Search functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-baguetteBox.run('.gallery');
+//runs baguetteBox
+window.addEventListener('load', function() {
+    baguetteBox.run('.gallery');
+  });
+
+//function for search functionality
+function liveSearch () {
+    let photos = document.querySelectorAll('a');
+    let search_query = document.getElementById("searchbox").value;
+
+    for (var i = 0; i < photos.length; i++) {
+        if(photos[i].getAttribute('data-caption').toLowerCase().includes(search_query.toLowerCase())) {
+            photos[i].classList.remove("is-hidden");
+        } else {
+            photos[i].classList.add("is-hidden");
+        }
+    }
+}
